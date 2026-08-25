7dfd114cc9c9ffb0ad403e477c9b356ec7af0e94
baarsie81-cloud <baarsie81@gmail.com>
2026-08-25 13:19:16 +0200

    remove early and hypothesis data

---
+++ migrations/0008_remove_early_hypothesis.sql
@@ -0,0 +1,6 @@
+-- Remove retired EARLY/Shadow and Hypothesis research state.
+DROP TABLE IF EXISTS early_shadow_deliveries;
+DROP TABLE IF EXISTS hypothesis_push_deliveries;
+DROP TABLE IF EXISTS hypothesis_events;
+DROP TABLE IF EXISTS hypothesis_score_snapshots;
+DROP TABLE IF EXISTS hypothesis_assets;
